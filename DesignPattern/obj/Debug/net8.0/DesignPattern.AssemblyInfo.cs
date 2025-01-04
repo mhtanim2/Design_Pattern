9522ffd65f3dc9f1d9cf52676f483f1bd587c2d7
mhtanim2 <mhtanim2@gmail.com>
2025-01-04 10:13:36 +0600

    added learing files

--- DesignPattern/obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
+++ DesignPattern/obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b970996067deb87420b1cc180101f15c2d1a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3179c0a862e3664cb78bcd444291b2fdcf6a57f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
